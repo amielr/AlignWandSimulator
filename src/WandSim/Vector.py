26a1f5612d380801fd2b2f892974b420a115333a
Amiel Rieger <amielr@yahoo.com>
2021-11-09 17:03:08 +0200

    initial Vector Class

---
+++ src/WandSim/Vector.py
@@ -0,0 +1,27 @@
+import math
+import numpy as np
+
+class Vector():
+    x, y, z = 0,0,0
+
+    def __init__(self,_x,_y,_z):
+        self.
+
+    def setX(self, _x):
+        self.x = _x
+
+    def setY(self,_y):
+        self.y = _y
+
+    def setZ(self,_z):
+        self.z = _z
+
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+
+    def getZ(self):
+        return self.z
+
